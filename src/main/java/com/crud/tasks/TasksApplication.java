4a94c9ee3d9cd88f5c4caea0921a6dcb11712a62
agnieszka <agammkrawczyk@gmail.com>
2018-08-27 20:43:06 +0200

    14.4 Controller

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -8,13 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TasksApplication {
 
 	public static void main(String[] args) {
-		TaskDto taskDto = new TaskDto((long) 1,"kot","I want to be a coder"  );
-		long id=taskDto.getId();
-		String title=taskDto.getTitle();
-		String content=taskDto.getContent();
 
-		System.out.println( title+ "  "+ id+ " "+ content+ "" );
 
-	//	SpringApplication.run(TasksApplication.class, args);
+	SpringApplication.run(TasksApplication.class, args);
 	}
 }
