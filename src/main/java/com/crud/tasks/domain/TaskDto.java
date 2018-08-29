97f468facec8bfd02aa83e4f3073595100cda82d
agnieszka <agammkrawczyk@gmail.com>
2018-08-29 23:49:15 +0200

    15.2

--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -3,11 +3,15 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class TaskDto {
     private Long id;
     private String title;
     private String content;
+
+
 }
