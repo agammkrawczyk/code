db88b749b2f9b65da13f09ae80aaa73a1ae422fa
agnieszka <agammkrawczyk@gmail.com>
2018-10-06 22:10:43 +0200

    18.3

--- src/main/java/com/crud/tasks/domain/BadgesDto.java
+++ src/main/java/com/crud/tasks/domain/BadgesDto.java
@@ -1,5 +1,4 @@
 package com.crud.tasks.domain;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
@@ -10,14 +9,9 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class CreatedTrelloCard {
-    @JsonProperty("id")
-    private String id;
-//    @JsonProperty("name")
-//    private String name;
-//    @JsonProperty("shortUrl")
-//    private String shortUrl;
-    @JsonProperty("badges")
-    private BadgesDto badges;
-
+public class BadgesDto {
+    @JsonProperty("votes")
+    private int votes;
+    @JsonProperty("attachmentsByType")
+    private AttachmentsByType attachments;
 }
