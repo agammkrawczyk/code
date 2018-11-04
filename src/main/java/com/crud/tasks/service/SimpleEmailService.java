0beb084ee51231c2d7fa1ab07b4a496e95aaf71f
agnieszka <agammkrawczyk@gmail.com>
2018-11-04 18:22:41 +0100

    19 mail

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.domain.Mail;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,22 +11,26 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SimpleEmailService {
-
-    private final static Logger LOGGER = LoggerFactory.getLogger( SimpleMailMessage.class );
     @Autowired
     private JavaMailSender javaMailSender;
 
-    public void send(final String receiverEmail, final String subject, final String message) {
-        try {
-            SimpleMailMessage mailMessage = new SimpleMailMessage();
-            mailMessage.setTo( receiverEmail );
-            mailMessage.setSubject( subject );
-            mailMessage.setText( message );
-            javaMailSender.send( mailMessage );
+    private final static Logger LOGGER = LoggerFactory.getLogger( SimpleMailMessage.class );
 
+    public void send(final Mail mail) {
+        LOGGER.info( "Statrting email preparation" );
+        try {
+            SimpleMailMessage mailMessage = createMailMessage( mail );
+            javaMailSender.send( mailMessage );
             LOGGER.info( "email has been sent" );
         } catch (MailException e) {
             LOGGER.error( "Failed to process email sending:", e.getMessage(), e );
         }
+    }
+    private SimpleMailMessage createMailMessage(final Mail mail){
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo( mail.getMailTo() );
+        mailMessage.setSubject( mail.getSubject());
+        mailMessage.setText( mail.getMessage());
+        return mailMessage;
     }
 }
